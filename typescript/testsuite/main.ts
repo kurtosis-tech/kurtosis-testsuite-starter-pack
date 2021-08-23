6e35e0f717211fb2ed530c60d78b863db6d806be
Ali Syed <alisyed0206@gmail.com>
2021-08-23 17:29:47 -0400

    adding docker files and main.ts which are needed to run build_and_run.sh successfully

---
+++ typescript/testsuite/main.ts
@@ -0,0 +1,23 @@
+//TODO - after porting of execution files
+
+//import { } from "testsuite/";
+"github.com/kurtosis-tech/kurtosis-testsuite-api-lib/golang/lib/execution"
+"github.com/sirupsen/logrus"
+"os"
+
+const successExitCode: number = 0;
+const failureExitCode: number = 1;
+
+// function main() {
+// 	// >>>>>>>>>>>>>>>>>>> REPLACE WITH YOUR OWN CONFIGURATOR <<<<<<<<<<<<<<<<<<<<<<<<
+// 	configurator := execution_impl.NewExampleTestsuiteConfigurator()
+// 	// >>>>>>>>>>>>>>>>>>> REPLACE WITH YOUR OWN CONFIGURATOR <<<<<<<<<<<<<<<<<<<<<<<<
+
+// 	suiteExecutor := execution.NewTestSuiteExecutor(configurator)
+// 	if err := suiteExecutor.Run(); err != nil {
+// 		logrus.Errorf("An error occurred running the test suite executor:")
+// 		fmt.Fprintln(logrus.StandardLogger().Out, err)
+// 		os.Exit(failureExitCode)
+// 	}
+// 	os.Exit(successExitCode)
+// }
