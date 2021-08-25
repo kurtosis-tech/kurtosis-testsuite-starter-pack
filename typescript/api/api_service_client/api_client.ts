7c6eaa225565fd0fab1f55cb8fcfabfa6eb3a02d
Ali Syed <alisyed0206@gmail.com>
2021-08-25 08:32:00 -0400

    fixing indentations

--- typescript/api/api_service_client/api_client.ts
+++ typescript/api/api_service_client/api_client.ts
@@ -12,50 +12,50 @@ const HEALTHCHECK_URL_SLUG: string = "health";
 const HEALTHY_VALUE: string = "healthy";
 
 class Person {
-	private readonly booksRead: number;
+    private readonly booksRead: number;
 
-	constructor(booksRead: number) {
+    constructor(booksRead: number) {
         this.booksRead = booksRead;
     }
 }
 
 class APIClient {
-	private readonly ipAddr: string;
-	private readonly port: number;
-	
-	constructor (ipAddr: string, port: number) {
-		this.ipAddr = ipAddr;
-		this.port = port;
-	}
+    private readonly ipAddr: string;
+    private readonly port: number;
+    
+    constructor (ipAddr: string, port: number) {
+        this.ipAddr = ipAddr;
+        this.port = port;
+    }
 
-	public async addPerson(id: number): Promise<Result<null, Error>> {
-		const url: string = this.getPersonUrlForId(id);
-		let resp: axios.AxiosResponse<any>;
-		try {
-			resp = await axios.default.post(url, null);
-		} catch(exception) {
-			return err(exception);
-		}
+    public async addPerson(id: number): Promise<Result<null, Error>> {
+        const url: string = this.getPersonUrlForId(id);
+        let resp: axios.AxiosResponse<any>;
+        try {
+            resp = await axios.default.post(url, null);
+        } catch(exception) {
+            return err(exception);
+        }
 
-		if (resp.status !== httpStatusCode.StatusCodes.OK) {
-			return err(new Error("Adding person with ID '" + id +  "' returned non-OK status code " + resp.status));
-		}
-		return ok(null);
-	}
+        if (resp.status !== httpStatusCode.StatusCodes.OK) {
+            return err(new Error("Adding person with ID '" + id +  "' returned non-OK status code " + resp.status));
+        }
+        return ok(null);
+    }
 
-	public async getPerson(id: number): Promise<Result<Person, Error>> {
-		const url: string = this.getPersonUrlForId(id);
-		let resp: axios.AxiosResponse<any>;
-		try {
-			resp = await axios.default.get(url);
-		} catch(exception) {
-			return err(exception);
-		}
+    public async getPerson(id: number): Promise<Result<Person, Error>> {
+        const url: string = this.getPersonUrlForId(id);
+        let resp: axios.AxiosResponse<any>;
+        try {
+            resp = await axios.default.get(url);
+        } catch(exception) {
+            return err(exception);
+        }
 
-		if (resp.status !== httpStatusCode.StatusCodes.OK) {
-			return err(new Error("Getting person with ID '" + id + "' returned non-OK status code " + resp.status));
-		}
-		const body: any = resp.data; //TODO (Ali) - this is a JSON object
+        if (resp.status !== httpStatusCode.StatusCodes.OK) {
+            return err(new Error("Getting person with ID '" + id + "' returned non-OK status code " + resp.status));
+        }
+        const body: any = resp.data; //TODO (Ali) - this is a JSON object
         
         //TODO (Ali) - What line below are doing:
         // 1) Taking JSON object and turning it into a string
@@ -63,91 +63,91 @@ class APIClient {
         // It feels like I'm doing and un-doing an action with these two commands though, but I'm certain I need parse
         // to build the Person object itself from the string
         let bodyString: string;
-		try {
-			bodyString = JSON.stringify(body);
-		} catch(jsonErr) {
-			return err(jsonErr);
-		}
+        try {
+            bodyString = JSON.stringify(body);
+        } catch(jsonErr) {
+            return err(jsonErr);
+        }
 
-		let person: Person;
-		try {
-			person = JSON.parse(bodyString); 
-		} catch(jsonErr) {
-			return err(jsonErr);
-		}
-		
-		return ok(person);
-	}
+        let person: Person;
+        try {
+            person = JSON.parse(bodyString); 
+        } catch(jsonErr) {
+            return err(jsonErr);
+        }
+        
+        return ok(person);
+    }
 
-	public async incrementBooksRead(id: number): Promise<Result<null, Error>> {
-		const url: string = "http://" + this.ipAddr + ":" + this.port + "/"+ INCREMENT_BOOKS_READ_ENDPOINT +"/" + id;
-		let resp: axios.AxiosResponse<any>;
-		try {
-			resp = await axios.default.post(url, null);
-		} catch(exception) {
-			return err(exception);
-		}
+    public async incrementBooksRead(id: number): Promise<Result<null, Error>> {
+        const url: string = "http://" + this.ipAddr + ":" + this.port + "/"+ INCREMENT_BOOKS_READ_ENDPOINT +"/" + id;
+        let resp: axios.AxiosResponse<any>;
+        try {
+            resp = await axios.default.post(url, null);
+        } catch(exception) {
+            return err(exception);
+        }
 
-		if (resp.status !== httpStatusCode.StatusCodes.OK) {
-			return err(new Error("Incrementing the books read of person with ID '" + id + "' returned non-OK status code " + resp.status + ""));
-		}
-		return ok(null);
-	}
+        if (resp.status !== httpStatusCode.StatusCodes.OK) {
+            return err(new Error("Incrementing the books read of person with ID '" + id + "' returned non-OK status code " + resp.status + ""));
+        }
+        return ok(null);
+    }
 
-	/*
-	Wait for healthy response
-	*/
-	public async waitForHealthy(retries: number, retriesDelayMilliseconds: number): Promise<Result<null, Error>> {
+    /*
+    Wait for healthy response
+    */
+    public async waitForHealthy(retries: number, retriesDelayMilliseconds: number): Promise<Result<null, Error>> {
 
-		const url: string = "http://"+this.ipAddr+":"+this.port+"/"+HEALTHCHECK_URL_SLUG;
-		let respResult: Result<axios.AxiosResponse<any>, Error> | null = null;
+        const url: string = "http://"+this.ipAddr+":"+this.port+"/"+HEALTHCHECK_URL_SLUG;
+        let respResult: Result<axios.AxiosResponse<any>, Error> | null = null;
 
-		for (let i = 0 ; i < retries ; i++) {
-			respResult = await this.makeHttpGetRequest(url);
-			if (respResult.isOk()) {
-				break;
-			}
-			await new Promise(resolve => setTimeout(resolve, retriesDelayMilliseconds));
-		}
+        for (let i = 0 ; i < retries ; i++) {
+            respResult = await this.makeHttpGetRequest(url);
+            if (respResult.isOk()) {
+                break;
+            }
+            await new Promise(resolve => setTimeout(resolve, retriesDelayMilliseconds));
+        }
 
         if (respResult === null) {
             return err(new Error("Expected a response or error wrapped around Result, but got null instead. Ensure that retries is greater than 0."));
         }
-		if (!respResult.isOk()){
-			return err(respResult.error);
-		}
+        if (!respResult.isOk()){
+            return err(respResult.error);
+        }
 
-		const resp: axios.AxiosResponse<any> = respResult.value;
+        const resp: axios.AxiosResponse<any> = respResult.value;
 
         let bodyString: string;
-		try {
-			bodyString = JSON.stringify(resp.data);
-		} catch(jsonErr) {
-			return err(jsonErr);
-		}
+        try {
+            bodyString = JSON.stringify(resp.data);
+        } catch(jsonErr) {
+            return err(jsonErr);
+        }
 
-		if (bodyString !== HEALTHY_VALUE) {
-			return err(new Error("Expected response body text '" + HEALTHY_VALUE + "' from endpoint '" + url + "' but got '" + bodyString +  "' instead"));
-		}
+        if (bodyString !== HEALTHY_VALUE) {
+            return err(new Error("Expected response body text '" + HEALTHY_VALUE + "' from endpoint '" + url + "' but got '" + bodyString +  "' instead"));
+        }
 
-		return ok(null);
-	}
+        return ok(null);
+    }
 
-	public getPersonUrlForId(id: number): string { //TODO (Ali) - since async functions use it, I might need to make this async
-		return "http://" + this.ipAddr + ":" + this.port + "/" + PERSON_ENDPOINT + "/" + id;
-	}
+    public getPersonUrlForId(id: number): string { //TODO (Ali) - since async functions use it, I might need to make this async
+        return "http://" + this.ipAddr + ":" + this.port + "/" + PERSON_ENDPOINT + "/" + id;
+    }
 
-	public async makeHttpGetRequest(url: string): Promise<Result<axios.AxiosResponse<any>, Error>>{
-		let resp: axios.AxiosResponse<any>;
-		try {
-			resp = await axios.default.get(url);
-		} catch(exception) {
-			return err(exception);
-		}
+    public async makeHttpGetRequest(url: string): Promise<Result<axios.AxiosResponse<any>, Error>>{
+        let resp: axios.AxiosResponse<any>;
+        try {
+            resp = await axios.default.get(url);
+        } catch(exception) {
+            return err(exception);
+        }
 
-		if (resp.status !== httpStatusCode.StatusCodes.OK) {
-			return err(new Error("Received non-OK status code: '" + resp.status + "'"));
-		}
-		return ok(resp);
-	}
+        if (resp.status !== httpStatusCode.StatusCodes.OK) {
+            return err(new Error("Received non-OK status code: '" + resp.status + "'"));
+        }
+        return ok(resp);
+    }
 }
