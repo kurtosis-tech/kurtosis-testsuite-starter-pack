7323740f60d0b378f60a93ec133406abc41cf0bf
Ali Syed <alisyed0206@gmail.com>
2021-08-27 22:47:52 -0400

    removed commented code

--- typescript/testsuite/execution_impl/example_testsuite_args.ts
+++ typescript/testsuite/execution_impl/example_testsuite_args.ts
@@ -1,18 +1,4 @@
 export interface ExampleTestsuiteArgs {
     readonly apiServiceImage: string;
     readonly datastoreServiceImage: string;
-    
-    //TODO - Remove if everything works
-    // constructor(apiServiceImage: string, datastoreServiceImage: string) {
-    //     this.apiServiceImage = apiServiceImage;
-    //     this.datastoreServiceImage = datastoreServiceImage;
-    // }
-    
-    // public getApiServiceImage(): string {
-    //     return this.apiServiceImage;
-    // }
-    
-    // public getDatastoreServiceImage(): string {
-    //     return this.datastoreServiceImage;
-    // }
 }
